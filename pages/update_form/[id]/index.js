438af0ac0faf70eae173d6c9b71fe8546b6aa40f
AlfiraPutri <nurlitaalfira@gmail.com>
2024-06-12 12:13:46 +0700

    bismi

--- pages/update_form/[id]/index.js
+++ pages/update_form/[id]/index.js
@@ -33,7 +33,7 @@ export default function UpdateFormPage() {
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const response = await fetch(`http://localhost:3000/api/users/${id}`);
+                const response = await fetch(`https://employee-crud-flax.vercel.app/${id}`);
                 const data = await response.json();
                 setUserData(data);
                 const { name, email, salary, date, status } = data;
